20cccfe9e04b81452ae56e9366c350d7aa88e19e
AndreitaBravo17 <100541544+AndreitaBravo17@users.noreply.github.com>
2022-05-17 21:29:01 -0500

    Add files via upload

---
+++ ejercicio4AB.py
@@ -0,0 +1,13 @@
+print('  ')
+print('--------------------------------------------------------------------------')
+print('                             Ejercicio 4')
+print('--------------------------------------------------------------------------')
+print('  ')
+print('----Tema: Ingresar la base y la altura de un triángulo y hallar su área.---')
+base=float(input('Ingrese el valor de la base: '))
+altura=float(input('Ingrese el valor de la altura: '))
+resultadoAreaTriangulo=(base*altura)/2
+
+print('El triángulo con base de ',base,' y altura de ', altura, ' forma un área de: ',resultadoAreaTriangulo)
+print('  ')
+print('--------------------------------------------------------------------------')
